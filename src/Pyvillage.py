2da1803f4ea4bbf7c33f37139d6366af7d5755b1
Rainier Ababao <rainier.ababao@gmail.com>
2015-05-22 01:51:11 -0500

    add pyvillage

---
+++ src/Pyvillage.py
@@ -0,0 +1,51 @@
+__author__ = 'rainierababao'
+
+from collections import Counter
+
+def slicing_practice(s, a, b, c, d):
+    return s[a:b+1] + " " + s[c:d+1]
+
+"""
+List comprehension!
+"""
+def conditions_and_loops(a, b):
+    return sum(i for i in range(a, b + 1) if i % 2 == 1)
+
+"""
+Bitwise-or!
+"""
+def conditions_and_loops_2(a, b):
+    return sum(range(a | 1, b + 1, 2))
+
+def even_lines(filename):
+    f = open(filename, 'r')
+    newFile = open('output.txt', 'w')
+    i = 0
+    for line in f:
+        if i % 2 == 1:
+            newFile.write(line)
+        i += 1
+
+def even_lines_2(filename):
+    with open(filename, 'r') as f:
+        print ''.join(f.readlines()[1::2])
+
+def frequencies(filename):
+    f = open(filename, 'r')
+    newFile = open('frequencies.txt', 'w')
+    frequencyDict = {}
+    for line in f:
+        for word in line.split():
+            if word in frequencyDict:
+                frequencyDict[word] += 1
+            else:
+                frequencyDict[word] = 1
+
+    for key in frequencyDict:
+        newFile.write(str(key + ' ' + str(frequencyDict[key]) + '\n'))
+
+def frequencies_2(filename):
+    with open(filename, 'r') as f:
+        from collections import Counter
+        for k, v in Counter(str(f.readlines()).split()).items():
+            print(k, v)
