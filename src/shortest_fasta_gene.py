5345f5a324cfdb7ece5ddb86a16db39dbf9bdded
Rainier Ababao <rainier.ababao@gmail.com>
2015-12-29 18:22:34 -0600

    Add shortest fasta gene

---
+++ src/shortest_fasta_gene.py
@@ -0,0 +1,32 @@
+from Bio import Entrez
+from Bio import SeqIO
+
+def get_genbank_ids(inp):
+    return inp.split(" ")
+
+def get_shortest_gene(genbank_ids):
+    """Hits the database twice :(
+    """
+    Entrez.email = "none@example.com"
+    # Fetches FASTA strings from the GenBank database
+    handle = Entrez.efetch(db='nucleotide', id=genbank_ids, rettype="fasta")
+    records = list(SeqIO.parse(handle, "fasta"))
+    ids_and_lens = []
+    for record in records:
+        ids_and_lens.append((record.id.split('|')[3].split('.')[0], len(record.seq)))
+    id_with_shortest_gene = min(ids_and_lens, key=lambda x: x[1])[0]
+    handle = Entrez.efetch(db='nucleotide', id=id_with_shortest_gene, rettype="fasta")
+    print handle.read()
+
+def get_shortest_gene_2(genbank_ids):
+    """Hits the database but involves more string manip
+    """
+    handle = Entrez.efetch(db="nucleotide", id=genbank_ids, rettype="fasta")
+    strands = handle.read().split('\n\n')[:-1]
+    print min(strands, key=lambda s: sum(len(x) for x in s.split('\n')[1:]))
+
+
+inp = "JX308803 JX469991 JX462669 GU292427 JX280897 JX317645 JX469983 NM_131329 JQ011270 NM_002037"
+ids = get_genbank_ids(inp)
+get_shortest_gene(ids)
+get_shortest_gene_2(ids)
