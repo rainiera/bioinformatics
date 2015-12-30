a10a1f57b4639b37c8b16d291c88b38eb4d77849
Rainier Ababao <rainier.ababao@gmail.com>
2015-12-29 18:38:26 -0600

    Move stuff to sandbox

---
+++ src/sandbox/is_permutation.py
@@ -0,0 +1,8 @@
+__author__ = 'rainierababao'
+
+from collections import Counter
+
+def is_permutation(thing1, thing2):
+    return Counter(thing1) == Counter(thing2)
+
+print is_permutation("LMAO", "MALO")
